3812d70e690ca8b9d3764dc7a59677134ca73c26
ssung2sin <ssung2sin@naver.com>
2023-11-18 13:23:03 +0900

    feat : 가격바 구현

--- SistFinalProject/src/main/java/sist/last/mapper/ProductMapperInter.java
+++ SistFinalProject/src/main/java/sist/last/mapper/ProductMapperInter.java
@@ -1,9 +1,12 @@
 package sist.last.mapper;
 
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ProductMapperInter {
 
     public int getAccommodation(String result);
+
+    public List<String> selectCategory();
 }
--- SistFinalProject/src/main/java/sist/last/service/ProductService.java
+++ SistFinalProject/src/main/java/sist/last/service/ProductService.java
@@ -1,12 +1,23 @@
 package sist.last.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sist.last.mapper.ProductMapperInter;
 
 @Service
-public class ProductService implements ProductServiceInter{
+public class ProductService implements ProductServiceInter {
 
     @Autowired
     ProductMapperInter mapperInter;
+
+    @Override
+    public int getAccommodation(String result) {
+        return 0;
+    }
+
+    @Override
+    public List<String> selectCategory() {
+        return mapperInter.selectCategory();
+    }
 }
