26e3530f68e31d769bfeab5ef7245afac4bcbd62
limchard <jksy6458@naver.com>
2023-11-20 09:27:34 +0900

    hj 231120

--- SistFinalProject/src/main/java/sist/last/controller/MainController.java
+++ SistFinalProject/src/main/java/sist/last/controller/MainController.java
@@ -3,6 +3,8 @@ package sist.last.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Scanner;
+
 @Controller
 public class MainController {
 
