aed1c57f7962da9b14ff7eb355a9fdb4d2456235
sunghyun <tjdgus9773@gmail.com>
2023-11-22 15:07:57 +0900

    feat: 결제완료 수정'

--- SistFinalProject/src/main/java/sist/last/controller/PaymentController.java
+++ SistFinalProject/src/main/java/sist/last/controller/PaymentController.java
@@ -10,7 +10,8 @@ public class PaymentController {
 	@GetMapping("/payment/pay-success")
     public ModelAndView paymentSuccess() {
         // payment.jsp로 이동
-        ModelAndView modelAndView = new ModelAndView("/payment/payment");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/payment/payment");
         return modelAndView;
     }
 }
