4e75f7a70640422da60ab4106faa36fb2207b2a8
limchard <jksy6458@naver.com>
2023-11-24 14:47:09 +0900

    feat chatting modify

--- SistFinalProject/src/main/java/sist/last/chat/SocketHandler.java
+++ SistFinalProject/src/main/java/sist/last/chat/SocketHandler.java
@@ -69,7 +69,11 @@
 //////////////////////////////////////////////////////////////////////
 package sist.last.chat;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -78,6 +82,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -96,6 +101,9 @@ public class SocketHandler extends TextWebSocketHandler {
 
     //HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 맵
     List<HashMap<String, Object>> rls = new ArrayList<>(); //웹소켓 세션을 담아둘 리스트 ---roomListSessions. 각 항목은 방 번호와 해당 방에 속한 세션들의 맵입니다.
+    private static final String FILE_UPLOAD_PATH="/chatsave";// 파일 업로드용
+    static int fileUploadIdx=0;
+    static String fileUploadSession="";
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
@@ -132,6 +140,55 @@ public class SocketHandler extends TextWebSocketHandler {
                         e.printStackTrace();
                     }
                 }
+            }
+        }
+    }
+
+    @Override
+    public  void handleBinaryMessage(WebSocketSession session, BinaryMessage message){
+        // 바이너리 메시지 발송
+        // BinaryMessage : 처음에 웹 소켓을 시작하면서 서버단을 구성할 때, 구현체 부분인 SocketHandler 부분을 구성할때 TextWebSocketHandler를 상속받고 메시지 타입에 따라 handleBinaryMessage 또는 handlerTextMessage가 실행된다고 했는데, 지금까지 JSON 형태의 String메시지만 전송하다보니 바이너리메시지는 다룰일이 없었습니다. 이번 장에서는 바이너리메시지를 사용하여 파일을 받고 서버에 저장도 하고 채팅방에 전송된 이미지를 표출하는 부분까지 구성해보자
+        ByteBuffer byteBuffer=message.getPayload();
+        String fileName="temp.jpg";
+        File dir=new File(FILE_UPLOAD_PATH);
+        if (!dir.exists()){
+            dir.mkdirs();
+        }
+        File file=new File(FILE_UPLOAD_PATH,fileName);
+        FileOutputStream out=null;
+        FileChannel outChannel=null;
+        try {
+            byteBuffer.flip(); // byteBuffer를 읽기 위해 세팅
+            out=new FileOutputStream(file,true); // 생성을 위해 OutputStream을 연다.
+            outChannel=out.getChannel(); // 채널을 열고
+            byteBuffer.compact(); // 파일을 복사한다.
+            outChannel.write(byteBuffer); // 파일을 쓴다.
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out!=null){
+                    out.close();
+                }
+                if (outChannel!=null){
+                    outChannel.close();
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        byteBuffer.position(0); // 파일을 저장하면서 position값이 변경되었으므로 0으로 초기화한다.
+        // 파일 쓰기가 끝나면 이미지를 발송한다.
+        HashMap<String,Object>temp=rls.get(fileUploadIdx);
+        for (String k:temp.keySet()){
+            if (k.equals("roomNumber")){
+                continue;
+            }
+            WebSocketSession wss=(WebSocketSession) temp.get(k);
+            try {
+                wss.sendMessage(new BinaryMessage(byteBuffer)); // 초기화된 버퍼를 발생힌다.
+            } catch (IOException e){
+                e.printStackTrace();
             }
         }
     }
@@ -195,4 +252,6 @@ public class SocketHandler extends TextWebSocketHandler {
         }
         return obj;
     }
+
+
 }
--- SistFinalProject/src/main/java/sist/last/controller/ChatController.java
+++ SistFinalProject/src/main/java/sist/last/controller/ChatController.java
@@ -13,12 +13,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Controller
-@RequestMapping("/chat")
+@Controller // 이 클래스가 Spring MVC의 컨트롤러 임을 나타내는 어노테이션이다.
+@RequestMapping("/chat") // 이 컨트롤러의 기본 URL 정보가 "/chat" 임을 지정합니다.
 public class ChatController {
 
-    List<Room> roomList = new ArrayList<Room>();
-    static int roomNumber = 0;
+    List<Room> roomList = new ArrayList<Room>(); // 방 정보를 저장하는 리스트
+    static int roomNumber = 0; // 방 번호 관리하는 변수, 0으로 초기화
 
     @RequestMapping("/chat")
     public ModelAndView chat() {
@@ -43,8 +43,9 @@ public class ChatController {
      * @param params
      * @return
      */
-    @RequestMapping("/createRoom")
-    public @ResponseBody List<Room> createRoom(@RequestParam HashMap<Object, Object> params){
+    @RequestMapping("/createRoom") // '/chat/createRoom'경로로  POST 요청이 오면 호출되는 메서드로, 새로운 채탕방을 생성합니다. 생성된 방은 'roomLIst'에 추가되고, 현재까지 생성된 모든 방의 목록이 클라이언트에게 반환됩니다.
+    public @ResponseBody List<Room> createRoom(@RequestParam HashMap<Object, Object> params){ // '@ResponseBody' : 메서드의 반환 값이 HTTP 응답 본문에 직접 포함되어야 함을 나타내는 어노테이션입니다.
+        // 'createRoom' 메서드 파라미터 :
         String roomName = (String) params.get("roomName");
         if(roomName != null && !roomName.trim().equals("")) {
             Room room = new Room();
