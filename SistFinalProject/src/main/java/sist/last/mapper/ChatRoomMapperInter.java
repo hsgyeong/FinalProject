3cf6300a6e1d1a94565109ff8632409c333ecd96
limchard <jksy6458@naver.com>
2023-11-30 14:04:22 +0900

    fix : add jalan

--- SistFinalProject/src/main/java/sist/last/mapper/ChatRoomMapperInter.java
+++ SistFinalProject/src/main/java/sist/last/mapper/ChatRoomMapperInter.java
@@ -9,8 +9,9 @@ import java.util.List;
 @Mapper
 public interface ChatRoomMapperInter {
     public int getMaxNum();
-    public void insertChatRoom(ChatDto chatDto);
-    public List<ChatDto> selectChatRoom();
+    public void insertChatRoom(ChatDto chatDto); // 채팅방 생성
+    public List<ChatDto> selectChatRoom(); // 채팅방 출력
+    public String getSenderId(String senderid); // 채팅방에 내 아이디로 된 채팅 방 있는지 찾기
 
 
 }
