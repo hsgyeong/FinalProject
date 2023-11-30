654319c4e01ff6eb79f5b71f8c0625bc6dcbc682
limchard <jksy6458@naver.com>
2023-11-30 17:33:58 +0900

    fix : 채팅 수정중... 살려줘 ㅠㅠ

--- SistFinalProject/src/main/java/sist/last/mapper/ChatRoomMapperInter.java
+++ SistFinalProject/src/main/java/sist/last/mapper/ChatRoomMapperInter.java
@@ -1,7 +1,6 @@
 package sist.last.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import sist.last.dto.ChatDto;
 import sist.last.dto.ChatRoomDto;
 
 import java.util.List;
@@ -9,9 +8,9 @@ import java.util.List;
 @Mapper
 public interface ChatRoomMapperInter {
     public int getMaxNum();
-    public void insertChatRoom(ChatDto chatDto); // 채팅방 생성
-    public List<ChatDto> selectChatRoom(); // 채팅방 출력
+    public void insertChatRoom(String senderid); // 채팅방 생성
+    public List<ChatRoomDto> selectChatRoom(); // 채팅방 출력
     public String getSenderId(String senderid); // 채팅방에 내 아이디로 된 채팅 방 있는지 찾기
-
+    public ChatRoomDto getChatRoom(String senderid); // senderid 로 정보 해당 정보 가져오기
 
 }
