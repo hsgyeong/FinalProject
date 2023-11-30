5170c3672ed49a13e7b75523c5ac6fd0952e7ceb
sunghyun <tjdgus9773@gmail.com>
2023-11-30 12:46:35 +0900

    feat: 관리자 숙소승인 페이징 처리까지 완료

--- SistFinalProject/src/main/java/sist/last/service/AdminMapperServiceInter.java
+++ SistFinalProject/src/main/java/sist/last/service/AdminMapperServiceInter.java
@@ -1,8 +1,14 @@
 package sist.last.service;
 
+import java.util.List;
 import java.util.Map;
+
+import sist.last.dto.AccomDto;
 
 public interface AdminMapperServiceInter {
 
-	public int AdminloginPassCheck(String admin_id,String admin_pass);
+	public int adminloginPassCheck(String admin_id,String admin_pass);
+	public void adminApprovalOfAccom(int accom_num);
+	public List<AccomDto> getAccomPagingList(int start, int perpage);
+	public int getTotalCount();
 }
